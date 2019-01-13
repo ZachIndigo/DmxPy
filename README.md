5e91b97d61919a5c5a21e6110f0d5ab6d3b54c84
itsb <itsb@users.noreply.github.com>
2019-01-12 21:49:37 -0800

    support both python 2 and 3 (thanks pySimpleDMX), add CLI, fix indexing bugs, update README, and make a pip installable package

--- README.md
+++ README.md
@@ -1,23 +1,43 @@
 ## DmxPy - Python Controller for USB - DMX devices
 
-This is a Python 3 port of [DmxPy](https://github.com/davepaul0/DmxPy).
+DmxPy is a super-lightweight Python library for controlling any USB-DMX device that is compatible with Enttec's USB DMX Pro, including all Dmxking ultraDMX devices.
 
-DmxPy is a super-lightweight Python library for controlling any USB-DMX device that is compatible with Enttec's DMXUSB Pro.  This includes all Dmxking ultraDMX devices.
+DmxPy requires [PySerial](http://pyserial.sourceforge.net) to work
 
-DmxPy requires PySerial to work - http://pyserial.sourceforge.net/
+### There is a console interface for basic sanity testing and validation
 
-To import:<br />
-<code>from DmxPy import DmxPy</code>
+For detailed usage instructions
 
-To initialize:<br />
-<code>dmx = DmxPy('serial port')</code>
-Where 'serial port' is where your device is located.
+    dmxpy -h
 
-To set a channel's value:<br />
-<code>dmx.setChannel(chan, value)</code>
-Where 'chan' and 'value' are integers representing the respective DMX channels and values to set!
+To black out (turn off) the lights when the DMX is on port COM4
 
-To push dmx changes to device:<br />
-<code>dmx.render()</code>
+    dmxpy --port COM4 -b
+
+To turn on all the lights at 50% (level 128)
+
+    dmxpy -p /dev/ttyUSB2 --level 128 -w
+
+To play the demo light pattern with the default level at 200
+
+    dmxpy -p COM2 -l 200 --demo
+
+### For more specific use cases use the DmxPy module
+To import
+
+    from dmxpy import DmxPy
+
+To initialize
+
+    dmx = DmxPy('serial port')
+where 'serial port' is where your device is located, .e.g, /dev/ttyUSB1 or COM3
+
+To set a channel's value
+
+    dmx.setChannel(chan, value)
+where 'chan' and 'value' are integers representing the respective DMX channels and values to set!
+
+To push dmx changes to device
+
+    dmx.render()
 You need to call this to update the device!
-
--- dmxpy/DmxPy.py
+++ dmxpy/DmxPy.py
@@ -1,39 +1,70 @@
-import serial, sys, time
-
-
-DMXOPEN = bytes([126])
-DMXCLOSE = bytes([231])
-DMXINTENSITY = bytes([6]) + bytes([1]) + bytes([2])				
-DMXINIT1 = bytes([3]) + bytes([2]) + bytes([0]) + bytes([0]) + bytes([0])
-DMXINIT2 = bytes([10]) + bytes([2]) + bytes([0]) + bytes([0]) + bytes([0])
-
+import serial, argparse, time
 
 class DmxPy:
-	def __init__(self, serialPort):
-		try:
-			self.serial = serial.Serial(serialPort, baudrate=57600)
-		except:
-			print("Error: could not open Serial Port")
-			sys.exit(0)
-		self.serial.write(DMXOPEN + DMXINIT1 + DMXCLOSE)
-		self.serial.write(DMXOPEN + DMXINIT2 + DMXCLOSE)
-		
-		self.dmxData = [bytes([0])] * 513   #128 plus "spacer".
-		
-		
-	def setChannel(self, chan, intensity):
-		if chan > 512: chan = 512
-		if chan < 0: chan = 0
-		if intensity > 255: intensity = 255
-		if intensity < 0: intensity = 0
-		self.dmxData[chan] = bytes([intensity])
-		
-		
-	def blackout(self):
-		for i in range(1, 512, 1):
-			self.dmxData[i] = bytes([0])
-		
-		
-	def render(self):
-		sdata = b''.join(self.dmxData)
-		self.serial.write(DMXOPEN + DMXINTENSITY + sdata + DMXCLOSE)
+    def __init__(self, serialPort, baudRate=57600, defaultLevel=255,dmxSize=512,levels=255):
+        self.defaultLevel = defaultLevel
+        self.levels = levels
+        self.dmxSize = dmxSize
+        try:
+            self.serial = serial.Serial(serialPort, baudrate=baudRate)
+        except:
+            print('Error: could not open Serial Port:', serialPort)
+            exit(1)
+        if self.defaultLevel<0 or self.defaultLevel>self.levels:
+            print('Light level must be 0-%d, not:'%self.levels, self.defaultLevel)
+            exit(1)
+        self.dmxData = [0] + [self.defaultLevel] * self.dmxSize
+
+    def setChannel(self, chan, level):
+        chan = self.dmxSize if chan > self.dmxSize else 1 if chan < 1 else chan # clip 1-self.dmxSize
+        level = self.levels if level > self.levels else 0 if level < 0 else level # clip 0-255
+        self.dmxData[chan] = level
+
+    def blackout(self):
+        self.dmxData = [0] + [0] * self.dmxSize
+
+    def whiteout(self):
+        self.dmxData = [0] + [self.defaultLevel] * self.dmxSize
+
+    def render(self):
+        dmxOpen = [0x7E]
+        dmxClose = [0xE7]
+        dmxIntensity = [6, (self.dmxSize+1) & 0xFF, (self.dmxSize+1)>>8 & 0xFF]
+        self.serial.write(bytearray(dmxOpen + dmxIntensity+ self.dmxData + dmxClose))
+
+def main():
+    parser = argparse.ArgumentParser(description='Control Enttec DMX USB Pro')
+    parser.add_argument('-r', '--rate', type=int, default=57600, help='baud rate for USB communication (default: 57600)')
+    parser.add_argument('-p', '--port', type=str, required=True, help='Serial(COM) port, e.g., /dev/ttyUSB1 or COM3')
+    parser.add_argument('-l', '--level', type=int, default=255, help='default level [0-255] of unspecified channels (default: 255)')
+    parser.add_argument('-s', '--size', type=int, default=512, help='DMX Size (default: 512)')
+    parser.add_argument('-b', '--blackout', action='store_true', help='Turn off all lights (level=0)')
+    parser.add_argument('-w', '--whiteout', action='store_true', help='Turn on all lights at level')
+    parser.add_argument('-d', '--demo', action='store_true', help='Play demo light pattern')
+    args = parser.parse_args()
+
+    dmx = DmxPy(args.port,baudRate=args.rate,defaultLevel=args.level,dmxSize=args.size)
+    if not args.blackout and not args.whiteout and not args.demo:
+        print('Select an action: [b]lackout, [w]hiteout, or [d]emo')
+        parser.print_usage()
+        exit(1)
+
+    if args.blackout:
+        dmx.blackout()
+        dmx.render()
+    elif args.whiteout:
+        dmx.whiteout()
+        dmx.render()
+    elif args.demo:
+        dmx.setChannel(1, 100)
+        dmx.setChannel(2, 50)
+        dmx.render()
+        time.sleep(5)
+        dmx.setChannel(3, 100)
+        dmx.render()
+        time.sleep(5)
+        dmx.blackout()
+        dmx.render()
+        time.sleep(5)
+        dmx.whiteout()
+        dmx.render()
