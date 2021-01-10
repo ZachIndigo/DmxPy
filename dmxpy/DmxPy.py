2c82b08ea3f7a904fd5b2294e1d9fefecd17969d
itsb <itsb@users.noreply.github.com>
2021-01-09 20:30:51 -0600

    PEP-8 cleanup

--- dmxpy/DmxPy.py
+++ dmxpy/DmxPy.py
@@ -1,49 +1,67 @@
-import serial, argparse, time
+import argparse
+import serial
+import time
+
 
 class DmxPy:
-    def __init__(self, serialPort, baudRate=57600, defaultLevel=255,dmxSize=512,levels=255):
-        self.defaultLevel = defaultLevel
+    def __init__(self, serial_port, baud_rate=57600, default_level=255,
+                 dmx_size=512, levels=255):
+        self.default_level = default_level
         self.levels = levels
-        self.dmxSize = dmxSize
+        self.dmx_size = dmx_size
         try:
-            self.serial = serial.Serial(serialPort, baudrate=baudRate)
-        except:
-            print('Error: could not open Serial Port:', serialPort)
+            self.serial = serial.Serial(serial_port, baudrate=baud_rate)
+        except serial.SerialException:
+            print('Error: could not open Serial Port:', serial_port)
             exit(1)
-        if self.defaultLevel<0 or self.defaultLevel>self.levels:
-            print('Light level must be 0-%d, not:'%self.levels, self.defaultLevel)
+        if self.default_level < 0 or self.default_level > self.levels:
+            print('Light level must be 0-%d, not:' % self.levels,
+                  self.default_level)
             exit(1)
-        self.dmxData = [0] + [self.defaultLevel] * self.dmxSize
+        self.dmxData = [0] + [self.default_level] * self.dmx_size
 
-    def setChannel(self, chan, level):
-        chan = self.dmxSize if chan > self.dmxSize else 1 if chan < 1 else chan # clip 1-self.dmxSize
-        level = self.levels if level > self.levels else 0 if level < 0 else level # clip 0-255
-        self.dmxData[chan] = level
+    def set_channel(self, channel, level):
+        channel = max(1, min(channel, self.dmx_size))  # clamp 1-self.dmx_size
+        level = max(0, min(level, 255))  # clamp 0-255
+        self.dmxData[channel] = level
 
     def blackout(self):
-        self.dmxData = [0] + [0] * self.dmxSize
+        self.dmxData = [0] + [0] * self.dmx_size
 
     def whiteout(self):
-        self.dmxData = [0] + [self.defaultLevel] * self.dmxSize
+        self.dmxData = [0] + [self.default_level] * self.dmx_size
 
     def render(self):
-        dmxOpen = [0x7E]
-        dmxClose = [0xE7]
-        dmxIntensity = [6, (self.dmxSize+1) & 0xFF, (self.dmxSize+1)>>8 & 0xFF]
-        self.serial.write(bytearray(dmxOpen + dmxIntensity+ self.dmxData + dmxClose))
+        dmx_open = [0x7E]
+        dmx_close = [0xE7]
+        dmx_intensity = [6, (self.dmx_size + 1) & 0xFF,
+                         (self.dmx_size + 1) >> 8 & 0xFF]
+        self.serial.write(
+            bytearray(dmx_open + dmx_intensity + self.dmxData + dmx_close))
+
 
 def main():
     parser = argparse.ArgumentParser(description='Control Enttec DMX USB Pro')
-    parser.add_argument('-r', '--rate', type=int, default=57600, help='baud rate for USB communication (default: 57600)')
-    parser.add_argument('-p', '--port', type=str, required=True, help='Serial(COM) port, e.g., /dev/ttyUSB1 or COM3')
-    parser.add_argument('-l', '--level', type=int, default=255, help='default level [0-255] of unspecified channels (default: 255)')
-    parser.add_argument('-s', '--size', type=int, default=512, help='DMX Size (default: 512)')
-    parser.add_argument('-b', '--blackout', action='store_true', help='Turn off all lights (level=0)')
-    parser.add_argument('-w', '--whiteout', action='store_true', help='Turn on all lights at level')
-    parser.add_argument('-d', '--demo', action='store_true', help='Play demo light pattern')
+    parser.add_argument('-r', '--rate', type=int, default=57600,
+                        help='baud rate for USB communication '
+                             '(default: 57600)')
+    parser.add_argument('-p', '--port', type=str, required=True,
+                        help='Serial(COM) port, e.g., /dev/ttyUSB1 or COM3')
+    parser.add_argument('-l', '--level', type=int, default=255,
+                        help='default level [0-255] of unspecified channels '
+                             '(default: 255)')
+    parser.add_argument('-s', '--size', type=int, default=512,
+                        help='DMX Size (default: 512)')
+    parser.add_argument('-b', '--blackout', action='store_true',
+                        help='Turn off all lights (level=0)')
+    parser.add_argument('-w', '--whiteout', action='store_true',
+                        help='Turn on all lights at level')
+    parser.add_argument('-d', '--demo', action='store_true',
+                        help='Play demo light pattern')
     args = parser.parse_args()
 
-    dmx = DmxPy(args.port,baudRate=args.rate,defaultLevel=args.level,dmxSize=args.size)
+    dmx = DmxPy(args.port, baud_rate=args.rate, default_level=args.level,
+                dmx_size=args.size)
     if not args.blackout and not args.whiteout and not args.demo:
         print('Select an action: [b]lackout, [w]hiteout, or [d]emo')
         parser.print_usage()
@@ -56,11 +74,11 @@ def main():
         dmx.whiteout()
         dmx.render()
     elif args.demo:
-        dmx.setChannel(1, 100)
-        dmx.setChannel(2, 50)
+        dmx.set_channel(1, 100)
+        dmx.set_channel(2, 50)
         dmx.render()
         time.sleep(5)
-        dmx.setChannel(3, 100)
+        dmx.set_channel(3, 100)
         dmx.render()
         time.sleep(5)
         dmx.blackout()
