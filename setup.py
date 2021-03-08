4235657ae661e774ed2a5ca34b490251812cd994
itsb <itsb@users.noreply.github.com>
2021-03-08 17:48:24 -0600

    new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='DmxPy',
-    version='0.4',
+    version='0.5',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
