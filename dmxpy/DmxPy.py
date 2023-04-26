1d99019b2067c4fd700bedb32d896f8f0b6216cc
zachir <zachir@librem.one>
2023-04-26 09:17:27 -0500

    update dmxpy to have channels

--- dmxpy/DmxPy.py
+++ dmxpy/DmxPy.py
@@ -1,8 +1,11 @@
 import argparse
 import time
+import math
 
 import serial
 from serial.tools import list_ports
+import tempfile
+import os
 
 
 def grep_ports(grep):
@@ -13,11 +16,10 @@ def grep_ports(grep):
 
 
 class DmxPy:
-    def __init__(self, serial_port=None, baud_rate=57600, default_level=255,
-                 dmx_size=512, levels=255, port_grep='0403:6001'):
-        self.default_level = default_level
-        self.levels = levels
+    def __init__(self, serial_port=None, baud_rate=57600, port_grep='0403:6001', channel_data='00', mod_type='set', dmx_size=64, path=''):
         self.dmx_size = dmx_size
+        self.dmx_cache_fname = os.path.join(tempfile.gettempdir(), 'dmx_cache.txt')
+
         if serial_port is None:
             serial_port = grep_ports(port_grep)
             print('Found DMX device matching %s at' % port_grep, serial_port)
@@ -26,22 +28,39 @@ class DmxPy:
         except serial.SerialException:
             print('Error: could not open Serial Port:', serial_port)
             exit(1)
-        if self.default_level < 0 or self.default_level > self.levels:
-            print('Light level must be 0-%d, not:' % self.levels,
-                  self.default_level)
-            exit(1)
-        self.dmxData = [0] + [self.default_level] * self.dmx_size
 
-    def set_channel(self, channel, level):
-        channel = max(1, min(channel, self.dmx_size))  # clamp 1-self.dmx_size
-        level = max(0, min(level, 255))  # clamp 0-255
-        self.dmxData[channel] = level
+        try:
+            if(path):
+                f = open(path, 'r')
+            else:
+                f = open(self.dmx_cache_fname, 'r')
+            with f:
+                self.dmxData = [0]
+                for line in f:
+                    dc = line[:-1]
+                    self.dmxData.append(int(dc))
+        except OSError:
+            print("No DMX cache file found. Initiate new channel array.")
+            self.dmxData = [0] + [0] * self.dmx_size
+
+        self.set_channels(channel_data, mod_type)
 
     def blackout(self):
         self.dmxData = [0] + [0] * self.dmx_size
 
-    def whiteout(self):
-        self.dmxData = [0] + [self.default_level] * self.dmx_size
+    def set_channels(self, channel_data, mod_type):
+        for x in range(0, len(channel_data), 2):
+            channel = math.floor(x/2) + 1
+            dmx_data = channel_data[x] + channel_data[x+1]
+            if(dmx_data != 'xx'):
+                if mod_type == 'up':
+                    self.dmxData[channel] += int(dmx_data, 16);
+                elif mod_type == 'down':
+                    self.dmxData[channel] -= int(dmx_data, 16)
+                else:
+                    self.dmxData[channel] = int(dmx_data, 16)
+                self.dmxData[channel] = min(255, self.dmxData[channel])
+                self.dmxData[channel] = max(0, self.dmxData[channel])
 
     def render(self):
         dmx_open = [0x7E]
@@ -51,6 +70,11 @@ class DmxPy:
         self.serial.write(
             bytearray(dmx_open + dmx_intensity + self.dmxData + dmx_close))
 
+        #write data to file 
+        with open(self.dmx_cache_fname, 'w') as filehandle:
+            self.dmxData.pop(0)
+            for dc in self.dmxData:
+                filehandle.write(f'{dc}\n')
 
 def main():
     parser = argparse.ArgumentParser(description='Control Enttec DMX USB Pro')
@@ -62,43 +86,20 @@ def main():
     parser.add_argument('-g', '--port-grep', type=str, default='0403:6001',
                         help='if port not specified attempt to auto-detect '
                              'serial matching grep (default: 0403:6001)')
-    parser.add_argument('-l', '--level', type=int, default=255,
-                        choices=range(0, 256),
-                        help='default level [0-255] of unspecified channels '
-                             '(default: 255)')
-    parser.add_argument('-s', '--size', type=int, default=512,
-                        help='DMX Size (default: 512)')
-    parser.add_argument('-b', '--blackout', action='store_true',
-                        help='Turn off all lights (level=0)')
-    parser.add_argument('-w', '--whiteout', action='store_true',
-                        help='Turn on all lights at level')
-    parser.add_argument('-d', '--demo', action='store_true',
-                        help='Play demo light pattern')
-    args = parser.parse_args()
+    parser.add_argument('-c', '--channel_data', type=str, default='00',
+                        help='channel data sent as pairs of hex, ie FFFF would be max level for channel 1 and 2')
+    parser.add_argument('-t', '--mod-type', type=str, default='set', help='Specify whether channel data is set, up or down (default: set)')
+    parser.add_argument('-s', '--size', type=int, default=64, help='DMX Size (default: 64)')
+    parser.add_argument('-f', '--path', type=str, default='', help='File to load DMX channel presets')
+    parser.add_argument('-b', '--blackout', action='store_true', help='Turn off all channels (level=0)')
 
-    dmx = DmxPy(args.port, baud_rate=args.rate, default_level=args.level,
-                dmx_size=args.size, port_grep=args.port_grep)
-    if not args.blackout and not args.whiteout and not args.demo:
-        print('Select an action: [b]lackout, [w]hiteout, or [d]emo')
-        parser.print_usage()
-        exit(1)
+    args = parser.parse_args()
+    dmx = DmxPy(args.port, baud_rate=args.rate, port_grep=args.port_grep, channel_data=args.channel_data, mod_type=args.mod_type, dmx_size=args.size, path=args.path)
 
     if args.blackout:
         dmx.blackout()
         dmx.render()
-    elif args.whiteout:
-        dmx.whiteout()
+    elif args.channel_data != '00':
         dmx.render()
-    elif args.demo:
-        dmx.set_channel(1, 100)
-        dmx.set_channel(2, 50)
-        dmx.render()
-        time.sleep(5)
-        dmx.set_channel(3, 100)
-        dmx.render()
-        time.sleep(5)
-        dmx.blackout()
-        dmx.render()
-        time.sleep(5)
-        dmx.whiteout()
-        dmx.render()
+    else:
+        print('Error, no channel data specified. Too blackout, choose -b.')
