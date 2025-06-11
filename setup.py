21a02835be4b497daa5ce767517e0a3a71899207
Zach Smith <zsmith@newhopechurch.tv>
2025-06-11 16:24:21 -0500

    Bump version 0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='DmxPy',
-    version='0.5',
+    version='0.5.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
