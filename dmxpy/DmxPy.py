8dd4a30c38d5aa162784e9254b76176d2dc16248
Zach Smith <zsmith@newhopechurch.tv>
2025-06-11 16:22:24 -0500

    Fix formatting

--- dmxpy/DmxPy.py
+++ dmxpy/DmxPy.py
@@ -1,5 +1,4 @@
 import argparse
-import time
 import math
 
 import serial
@@ -16,9 +15,12 @@ def grep_ports(grep):
 
 
 class DmxPy:
-    def __init__(self, serial_port=None, baud_rate=57600, port_grep='0403:6001', channel_data='00', mod_type='set', dmx_size=64, path=''):
+    def __init__(self, serial_port=None, baud_rate=57600,
+                 port_grep='0403:6001', channel_data='00', mod_type='set',
+                 dmx_size=64, path=''):
         self.dmx_size = dmx_size
-        self.dmx_cache_fname = os.path.join(tempfile.gettempdir(), 'dmx_cache.txt')
+        self.dmx_cache_fname = os.path.join(
+            tempfile.gettempdir(), 'dmx_cache.txt')
 
         if serial_port is None:
             serial_port = grep_ports(port_grep)
@@ -30,7 +32,7 @@ class DmxPy:
             exit(1)
 
         try:
-            if(path):
+            if (path):
                 f = open(path, 'r')
             else:
                 f = open(self.dmx_cache_fname, 'r')
@@ -52,9 +54,9 @@ class DmxPy:
         for x in range(0, len(channel_data), 2):
             channel = math.floor(x/2) + 1
             dmx_data = channel_data[x] + channel_data[x+1]
-            if(dmx_data != 'xx'):
+            if (dmx_data != 'xx'):
                 if mod_type == 'up':
-                    self.dmxData[channel] += int(dmx_data, 16);
+                    self.dmxData[channel] += int(dmx_data, 16)
                 elif mod_type == 'down':
                     self.dmxData[channel] -= int(dmx_data, 16)
                 else:
@@ -70,11 +72,12 @@ class DmxPy:
         self.serial.write(
             bytearray(dmx_open + dmx_intensity + self.dmxData + dmx_close))
 
-        #write data to file 
+        # write data to file
         with open(self.dmx_cache_fname, 'w') as filehandle:
             self.dmxData.pop(0)
             for dc in self.dmxData:
                 filehandle.write(f'{dc}\n')
+
 
 def main():
     parser = argparse.ArgumentParser(description='Control Enttec DMX USB Pro')
@@ -87,14 +90,22 @@ def main():
                         help='if port not specified attempt to auto-detect '
                              'serial matching grep (default: 0403:6001)')
     parser.add_argument('-c', '--channel_data', type=str, default='00',
-                        help='channel data sent as pairs of hex, ie FFFF would be max level for channel 1 and 2')
-    parser.add_argument('-t', '--mod-type', type=str, default='set', help='Specify whether channel data is set, up or down (default: set)')
-    parser.add_argument('-s', '--size', type=int, default=64, help='DMX Size (default: 64)')
-    parser.add_argument('-f', '--path', type=str, default='', help='File to load DMX channel presets')
-    parser.add_argument('-b', '--blackout', action='store_true', help='Turn off all channels (level=0)')
+                        help='channel data sent as pairs of hex, ie FFFF \
+                                would be max level for channel 1 and 2')
+    parser.add_argument('-t', '--mod-type', type=str, default='set',
+                        help='Specify whether channel data is set, up or down \
+                                (default: set)')
+    parser.add_argument('-s', '--size', type=int, default=64, help='DMX Size \
+            (default: 64)')
+    parser.add_argument('-f', '--path', type=str, default='', help='File to \
+            load DMX channel presets')
+    parser.add_argument('-b', '--blackout', action='store_true', help='Turn \
+            off all channels (level=0)')
 
     args = parser.parse_args()
-    dmx = DmxPy(args.port, baud_rate=args.rate, port_grep=args.port_grep, channel_data=args.channel_data, mod_type=args.mod_type, dmx_size=args.size, path=args.path)
+    dmx = DmxPy(args.port, baud_rate=args.rate, port_grep=args.port_grep,
+                channel_data=args.channel_data, mod_type=args.mod_type,
+                dmx_size=args.size, path=args.path)
 
     if args.blackout:
         dmx.blackout()
